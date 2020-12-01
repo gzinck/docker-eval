0765a8e0e3f81b8286f0321f183d1e24eeba375e
Nikhita Joshi <nikhita.joshi@gmail.com>
2020-12-01 14:43:38 -0500

    Update run-all.sh to work on Windows

--- run-all.sh
+++ run-all.sh
@@ -10,7 +10,7 @@
 # ". SOME_FILE").
 
 ENV_ENABLED=0 # To enable, change to 1
-PYTHON='python3' # Change to 'python' if this doesn't work
+PYTHON='python' # Change to 'python' if this doesn't work
 SOURCE='source'
 ENV_ACTIVATE='venv/bin/activate'
 
@@ -75,17 +75,20 @@ for n in {0..9}; do
 	fi
 
 	# Get the most recent item, move it
-	mv output/results.csv "$OUT/native-$n.csv"
+	mv "$(pwd)/output/results.csv" "$OUT/native-$n.csv"
+
+	# not sure why tf we have to do this but in Docker (Windows), it won't create a new results.csv file if it doesn't exist
+	touch "$(pwd)/output/results.csv"
 
 	# Docker trial
 	echo "Performing trial $n---docker"
-	docker run -it --rm \
+	docker run -i --rm \
         --mount type=bind,source="$(pwd)/benchmarks",target=/benchmarks \
         --mount type=bind,source="$(pwd)/output",target=/output \
         opencv-py:1.0 python3 benchmark.py
 	
 	# Get the most recent item, move it
-	mv output/results.csv "$OUT/docker-$n.csv"
+	mv "$(pwd)/output/results.csv" "$OUT/docker-$n.csv"
 done
 
 echo "Experiment complete! See the results in the folder $OUT"
